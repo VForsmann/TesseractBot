fd5bae53cceeb6282b13dc9bd7ef2e48e68e69dd
Vitus Forsmann <vitus.forsmann@hotmail.de>
2020-02-11 15:32:52 +0100

    Update walk_straight.py

--- walk_straight.py
+++ walk_straight.py
@@ -109,7 +109,7 @@ try:
 
                     d = pytesseract.image_to_data(img, output_type='data.frame')
 
-                    cv2.imshow('screen', img)
+                    #cv2.imshow('screen', img)
                     if (cv2.waitKey(1) & 0xFF) == ord('q'):
                         cv2.destroyAllWindows()
                         break
